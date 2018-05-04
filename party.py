5604f5a08df9491e458710baed3869cca01f9aa0
Hackbright Student <no-reply@hackbrightacademy.com>
2018-05-03 20:55:30 -0700

    trying to add feature to add games to list; needs work

--- party.py
+++ party.py
@@ -3,7 +3,7 @@
 
 from flask import Flask, session, render_template, request, flash, redirect
 from flask_debugtoolbar import DebugToolbarExtension
-from model import Game, connect_to_db
+from model import Game, connect_to_db, db
 
 app = Flask(__name__)
 app.secret_key = "SECRETSECRETSECRET"
@@ -37,6 +37,22 @@ def games():
             return render_template("games.html", games=games)
     except:
         return redirect("/")
+
+@app.route("/add_game", methods=['POST'])
+def add_game():
+    """ Add a game to the list that you plan to bring to the party """
+
+    # For some reason these arguments are being read as "Null", either when
+    # coming in from the POST request, or when getting passed to SQL
+    game_name = request.form.get('gameName')
+    game_desc = request.form.get('gameDesc')
+
+    db.session.add(Game(name=game_name, description=game_desc))
+    db.session.commit()
+
+    all_games_list = db.session.query(games).all()
+
+    return all_games_list
 
 
 if __name__ == "__main__":
